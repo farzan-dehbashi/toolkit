4cd756a64283977583af0969534146ccacc0132d
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2021-09-20 11:52:53 -0400

    test added

---
+++ testing.py/test_lib.py
@@ -0,0 +1,16 @@
+from jovian.pythondsa import evaluate_test_cases
+
+tests = [{'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 7}, 'output': 3},
+ {'input': {'cards': [13, 11, 10, 7, 4, 3, 1, 0], 'query': 1}, 'output': 6},
+ {'input': {'cards': [4, 2, 1, -1], 'query': 4}, 'output': 0},
+ {'input': {'cards': [3, -1, -9, -127], 'query': -127}, 'output': 3},
+ {'input': {'cards': [6], 'query': 6}, 'output': 0},
+ {'input': {'cards': [9, 7, 5, 2, -9], 'query': 4}, 'output': -1},
+ {'input': {'cards': [], 'query': 7}, 'output': -1},
+ {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0], 'query': 3},
+  'output': 7},
+ {'input': {'cards': [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0],
+   'query': 6},
+  'output': 2}]
+
+evaluate_test_cases(<func_name>, tests)
