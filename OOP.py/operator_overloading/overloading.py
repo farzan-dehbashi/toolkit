426cfae498cb26cd932404f9b7bea49c2e2d0186
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2021-11-23 12:43:50 -0500

    .

--- OOP.py/operator_overloading/overloading.py
+++ OOP.py/operator_overloading/overloading.py
@@ -11,7 +11,30 @@ class Point:
     def __str__(self):
         return f"({self.x},{self.y})"
 
-p1 = Point(1, 2)
+    def __bool__(self):
+        if self.x == 1:
+            return True
+        else:
+            return False
+    def __next__(self):
+        return (1,1)
+
+    def __call__(self, num):
+        self.x = num
+        print(self.x)
+        return self.x
+
+p1 = Point(3, 2)
 p2 = Point(2, 3)
+class Num_controller:
+    def __init__(self, x_val): # this is x_val of the point num controller is controlling
+        self.point = Point(x_val)
 
 print(p1+p2)
+print(bool(p1))
+print(str(p1))
+print(p1.__next__())
+print(next(p1))
+p1(2) # this is because we have overloaded call method, therefore, we act like an method to our object
+num_manager = Num_controller(12)
+num_manager.point(13)
