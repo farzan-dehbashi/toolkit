928d789a58d89fb580c4a3f9d2f899b3a9defe68
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-02-03 13:50:53 -0500

    .

--- interview_questions/longest_subset_palindrom.py
+++ interview_questions/longest_subset_palindrom.py
@@ -12,7 +12,7 @@ def find_root_palindroms(s):
 
 
 def find_longest_palindrom(s, singles, doubles):
-    longest_pal = (0 , 0)
+    longest_pal = (0, 0)
     for single_pal_i in singles:
         l = single_pal_i - 1
         r = single_pal_i + 1
