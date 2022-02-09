61b3d84f694983202be57b1c5e797406c5cdef30
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-02-09 01:08:21 -0500

    .

---
+++ react/my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          Hi this is Farzan and this is my first react project!
+        </p>
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
