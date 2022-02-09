38966d4a26b3e1638e3c0db5864e8bf80434d685
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-02-09 16:26:13 -0500

    arr

--- bash/bash_scripting/script.sh
+++ bash/bash_scripting/script.sh
@@ -87,12 +87,14 @@ math(){
 }
 
 arrays(){
-    var1=1
-    var2=2
-    var3=3
-    arr=()
+    var1=10
+    var2=20
+    var3=30
+    arr=($var1 $var2 $var3)
+    echo ${arr[@]}
+    echo ${arr[2]}
 }
 ############
-math
+arrays
 
 
