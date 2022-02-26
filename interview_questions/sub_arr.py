98ba4df9c9df0e9452ad3e3bc9f47e50c5b7d98b
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-02-26 13:06:41 -0500

    .

---
+++ interview_questions/sub_arr.py
@@ -0,0 +1,5 @@
+def sub_arr(arr):
+    for c in range(0, len(arr), 1):
+        print(c)
+
+print(sub_arr([0,0,1,0,1,0,1,1]))
--- react/hello_world/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-const element = <h1>Hello world</h1> ;
-// saves changes
-// console.log(element) 
-
-//render the object to show it
-ReactDom.render(element, document.getElementById('root'));
