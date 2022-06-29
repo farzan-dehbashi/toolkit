ee4c63bd45951b522411d03df04649c8d20bc2be
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-06-29 17:06:57 -0400

    First sms sent

--- api/sms/send.py
+++ api/sms/send.py
@@ -1,13 +1,14 @@
 from twilio.rest import Client 
  
 account_sid = 'AC4107dc143bfc240213e67f779e703ce2' 
-auth_token = '[79d97bf7985186929b6f1cc8490e14fe]' 
+auth_token = '[]' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create(  
                               messaging_service_sid='MG05d664871f4c94eef7ebc4524fdb736d', 
-                              body='Just my first note',      
+                              body='Hello from me, Farzan',      
                               to='+15195891234' 
                           ) 
  
 print(message.sid)
+79d97bf7985186929b6f1cc8490e14fe
