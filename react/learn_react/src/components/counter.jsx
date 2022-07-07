49367c06d083cbebf3d56a4cddd13fa3ed180547
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-07-07 18:38:07 -0400

    .

--- react/learn_react/src/components/counter.jsx
+++ react/learn_react/src/components/counter.jsx
@@ -1,36 +1,45 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 1,
+  state = {
+    count: 0,
+  };
 
-    };
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
 
-    styles = {
-        fontSize: 10,
-        fontWeight: "bold"
-    };
+  handleIncreament = () => {
+    this.state.count += 1;
+    this.setState({cout: this.state.count + 1})
+  }
 
   render() {
-
     return (
       <div>
         <span className={this.getBadgeClasses()} style={this.styles}>
-            {this.formatCount()}
+          {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncreament}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
 
-    getBadgeClasses() {
-        let classes = "m-2 btn btn-";
-        classes += this.state.count === 0 ? "warning" : "info";
-    }
+  getBadgeClasses() {
+    let classes = "m-2 btn btn-";
+    classes += this.state.count === 0 ? "warning" : "info";
+    return classes;
+  }
 
-  formatCount(){
-    const {count} = this.state;
-    return count === 0 ? 'Zero' : count;
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
