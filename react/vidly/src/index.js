1175f41349c526ad6f27c16498431a82c1513665
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-07-12 18:24:35 -0400

    .

--- react/vidly/src/index.js
+++ react/vidly/src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
