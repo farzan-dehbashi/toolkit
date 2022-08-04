28705fb7cb7ae67d423ef66208b92f2d8be8e431
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-08-04 17:35:45 -0400

    .

--- api/amber_script/python/export_stl_amberscript.py
+++ api/amber_script/python/export_stl_amberscript.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.amberscript.com/api/jobs/export-srt"
 
-querystring = {"jobId":"62c447275e35744086521521", "apiKey":"26fe6e92ac6ec36f7cb5c311dd36436fd"}
+querystring = {"jobId":"62ea86a460c0d9463c1197f8", "apiKey":"26fe6e92ac6ec36f7cb5c311dd36436fd"}
 
 payload = ""
 response = requests.request("GET", url, data=payload, params=querystring)
--- api/amber_script/python/status_amberscript.py
+++ api/amber_script/python/status_amberscript.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://api.amberscript.com/api/jobs/status"
 
-querystring = {"jobId":"62c31f2dc168d83541d4d4b7","apiKey":"26fe6e92ac6ec36f7cb5c311dd36436fd"}
+querystring = {"jobId":"62e96a8d2ba2ee71d3a6f0ac","apiKey":"26fe6e92ac6ec36f7cb5c311dd36436fd"}
 
 payload = ""
 response = requests.request("GET", url, data=payload, params=querystring)
