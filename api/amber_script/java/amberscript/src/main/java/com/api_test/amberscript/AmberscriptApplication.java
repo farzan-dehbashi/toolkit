3891c851fc7a9f42572254813bf04933dc20cd33
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2022-08-05 17:19:20 -0400

    .

--- api/amber_script/java/amberscript/src/main/java/com/api_test/amberscript/AmberscriptApplication.java
+++ api/amber_script/java/amberscript/src/main/java/com/api_test/amberscript/AmberscriptApplication.java
@@ -4,6 +4,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
 import java.io.File;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpUriRequest;
@@ -12,63 +15,80 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.FileBody;
-// import java.util.HashMap;
-// import java.util.Map;
-// import java.util.Scanner;
-// import java.util.concurrent.CompletableFuture;
-// import java.io.IOException;
-// import java.net.URI;
 
-// import okhttp3.MediaType;
-// import okhttp3.OkHttpClient;
-// import okhttp3.Request;
-// import okhttp3.RequestBody;
-// import okhttp3.Response;
-
-// import org.apache.http.HttpEntity;
-// import org.apache.http.HttpResponse;
-// import org.apache.http.client.HttpClient;
-// // import org.apache.http.client.entity.EntityBuilder;
-// import org.apache.http.client.methods.HttpGet;
-// import org.apache.http.client.methods.HttpPost;
-// import org.apache.http.client.utils.URIBuilder;
-// import org.apache.http.entity.ContentType;
-// import org.apache.http.impl.client.CloseableHttpClient;
-// import org.apache.http.impl.client.HttpClientBuilder;
-// import org.apache.http.impl.client.HttpClients;
-// import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // import org.springframework.boot.autoconfigure.batch.BatchProperties.Job;
 
 @SpringBootApplication
 public class AmberscriptApplication {
 	public static void main(String[] args) {
-		check();
-		check();
-		get();
+		// AmberscriptApiResponse checkResponse = check("62ec377b5e0b80225efcbb00",
+		// "26fe6e92ac6ec36f7cb5c311dd36436fd");
+		AmberscriptGetCaptionResponse capResposne = getCaption("62e96a8d2ba2ee71d3a6f0ac",
+				"26fe6e92ac6ec36f7cb5c311dd36436fd",
+				"srt");
+		System.out.println(capResposne.caption);
 	}
 
-	public static void get() {
-		try {
-			String jobId = "62ec377b5e0b80225efcbb00";
-			String apiKey = "26fe6e92ac6ec36f7cb5c311dd36436fd";
-			String url = "https://qs.amberscript.com/jobs/export-stl?jobId=" + jobId + "&apiKey=" + apiKey;
-		} catch (Exception e) {
-
-		}
+	private static class AmberscriptGetCaptionResponse {
+		int responseCode;
+		String caption;
 	}
 
-	public static void check() {
+	public static AmberscriptGetCaptionResponse getCaption(String jobId, String apiKey, String captionType) {
+		int status = -1;
+		String captionStr = "";
 		try {
-			String jobId = "62ec377b5e0b80225efcbb00";
-			String apiKey = "26fe6e92ac6ec36f7cb5c311dd36436fd";
-			String url = "https://api.amberscript.com/api/jobs/export-srt?jobId=" + jobId + "&apiKey=";
+			String url = "https://qs.amberscript.com/jobs/export-" + captionType + "?jobId=" + jobId + "&apiKey="
+					+ apiKey;
 			CloseableHttpClient client = HttpClients.createDefault();
 			HttpGet get = new HttpGet(url);
 			HttpResponse response = client.execute(get);
-			int i = 1;
+			HttpEntity httpEntity = response.getEntity();
+			status = response.getStatusLine().getStatusCode();
+			if (status != 200) {
+				throw new Exception("Get status http response code is" + status);
+			} else {
+				captionStr = EntityUtils.toString(httpEntity, "UTF-8");
+			}
 		} catch (Exception e) {
 		}
+		AmberscriptGetCaptionResponse response = new AmberscriptGetCaptionResponse();
+		response.responseCode = status;
+		response.caption = captionStr;
+		return response;
+	}
+
+	private static class AmberscriptCheckResponse {
+		int responseCode;
+		String captionRequestStatus;
+	}
+
+	public static AmberscriptCheckResponse check(String jobId, String apiKey) {
+		int status = -1;
+		String translationStatus = "Null";
+		try {
+			String url = "https://qs.amberscript.com/jobs/status?jobId=" + jobId + "&apiKey=" + apiKey;
+			// String url =
+			// "https://qs.amberscript.com/jobs/status?jobId=62ec377b5e0b80225efcbb00&apiKey=26fe6e92ac6ec36f7cb5c311dd36436fd";
+			CloseableHttpClient client = HttpClients.createDefault();
+			HttpGet get = new HttpGet(url);
+			HttpResponse response = client.execute(get);
+			HttpEntity httpEntity = response.getEntity();
+			String responseString = EntityUtils.toString(httpEntity, "UTF-8");
+			status = response.getStatusLine().getStatusCode();
+			if (status != 200) {
+				throw new Exception("Get status http response code is" + status);
+			} else {
+				JSONObject obj = new JSONObject(responseString);
+				translationStatus = obj.getJSONObject("jobStatus").getString("status");
+			}
+		} catch (Exception e) {
+		}
+		AmberscriptCheckResponse response = new AmberscriptCheckResponse();
+		response.responseCode = status;
+		response.captionRequestStatus = translationStatus;
+		return response;
 	}
 
 	public static void send() {
