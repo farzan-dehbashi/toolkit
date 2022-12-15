455c4cd62edf318b10b3ba50bb8e32869e649236
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2022-12-15 14:34:28 -0500

    .

---
+++ python_mosh/abstract_class.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractclassmethod
+
+class Stream(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractclassmethod
+    def read(self):
+        pass
+
+    @abstractclassmethod
+    def write(self):
+        pass
