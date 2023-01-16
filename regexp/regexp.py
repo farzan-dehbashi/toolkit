949e89d8460dcbe0bbb6e27ffe918eaec7d1102f
farzanDb <farzan.dehbashi95@gmail.com>
2023-01-16 14:57:20 -0500

    .

---
+++ regexp/regexp.py
@@ -0,0 +1,25 @@
+import re
+
+text = '''abcdefghijklmnopqrstuvwxyz
+ABCDEFGHIJKLMNOPQRSTUVWXYZ
+1234567890
+
+Ha HaHa
+
+MetaCharacters (Need to be escaped):
+. ^ $ * + ? { } [ ] \ | ( )
+
+coreyms.com
+
+321-555-4321
+123.555.1234
+
+Mr. Schafer
+Mr Smith
+Ms Davis
+Mrs. Robinson
+Mr. T
+'''
+
+sentence = 'Start a sentence and then bring it to an end'
+
