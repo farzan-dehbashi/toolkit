ce96048b7f54fd4501bef82970702ae29e842472
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2026-07-02 18:34:44 -0400

    feat: add shell sort implementation (#21)

---
+++ algorithms/sort/shell_sort.py
@@ -0,0 +1,16 @@
+def shell_sort(arr):
+    gap = len(arr) // 2
+    while gap > 0:
+        for i in range(gap, len(arr)):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+
+if __name__ == "__main__":
+    print(shell_sort([12, 34, 54, 2, 3]))
