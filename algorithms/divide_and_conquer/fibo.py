8aaa971a77a829a4056cfa569d7efbee8e20aec8
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2026-07-02 18:29:26 -0400

    fix: fibonacci base case fibo(0) should return 0 not 1

--- algorithms/divide_and_conquer/fibo.py
+++ algorithms/divide_and_conquer/fibo.py
@@ -1,9 +1,12 @@
 def fibo(n):
-    if n == 1:
-        return 1
-    elif n == 0:
+    if n == 0:
+        return 0
+    elif n == 1:
         return 1
     else:
-        return fibo(n-1) + fibo(n-2)
+        return fibo(n - 1) + fibo(n - 2)
 
-print(fibo(3))
+
+if __name__ == "__main__":
+    for i in range(8):
+        print(f"fibo({i}) = {fibo(i)}")
