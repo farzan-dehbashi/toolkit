58891dddab76f774bd86f64d618dfc106502293a
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2026-07-02 19:16:29 -0400

    feat: add Union Find (Disjoint Set) with path compression and union by rank

---
+++ DS/union_find.py
@@ -0,0 +1,34 @@
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        self.components = n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])  # path compression
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.rank[px] < self.rank[py]:
+            px, py = py, px
+        self.parent[py] = px
+        if self.rank[px] == self.rank[py]:
+            self.rank[px] += 1
+        self.components -= 1
+        return True
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+
+if __name__ == "__main__":
+    uf = UnionFind(5)
+    uf.union(0, 1)
+    uf.union(1, 2)
+    print(uf.connected(0, 2))   # True
+    print(uf.connected(0, 3))   # False
+    print(uf.components)        # 3
