a7a01fdcdd0f0c6db6aad58204351868bcb1c873
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2026-07-02 18:29:55 -0400

    feat: add two sum solution using hash map

---
+++ algorithms/hash_map/two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in seen:
+            return [seen[complement], i]
+        seen[num] = i
+    return []
+
+
+if __name__ == "__main__":
+    print(two_sum([2, 7, 11, 15], 9))   # [0, 1]
+    print(two_sum([3, 2, 4], 6))        # [1, 2]
