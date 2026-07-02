8a4e0bcd3eb2d39dd004cb8b014f36cc7676187c
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2026-07-02 18:28:47 -0400

    fix: binary search off-by-one in main() call and add main guard

--- algorithms/search/binary_search.py
+++ algorithms/search/binary_search.py
@@ -1,23 +1,17 @@
 def binary_search(l, r, arr, val):
     if r >= l:
-        m = (l+r) // 2
-
+        m = (l + r) // 2
         if arr[m] == val:
             return m
-
         elif val > arr[m]:
-            return binary_search(m+1, r, arr, val)
-
-        elif val < arr[m]:
-            return binary_search(l, m-1, arr, val)
-
+            return binary_search(m + 1, r, arr, val)
+        else:
+            return binary_search(l, m - 1, arr, val)
     else:
-        print('value '+str(val)+' not found')
+        print('value ' + str(val) + ' not found')
+        return -1
 
 
-def main():
-    arr = [1,2,3,4,5,6,7,8,12,19]
-    print(binary_search(0,len(arr),  arr, 12))
-
-
-main()
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 12, 19]
+    print(binary_search(0, len(arr) - 1, arr, 12))
