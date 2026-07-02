2a8da9d2ff96c1815ba0ebaef830b6f30011d373
farzan-dehbashi <farzan.dehbashi95@gmail.com>
2026-07-02 18:29:48 -0400

    feat: add tower of hanoi recursive solution

---
+++ algorithms/divide_and_conquer/tower_of_hanoi.py
@@ -0,0 +1,11 @@
+def tower_of_hanoi(n, source, target, auxiliary):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {target}")
+        return
+    tower_of_hanoi(n - 1, source, auxiliary, target)
+    print(f"Move disk {n} from {source} to {target}")
+    tower_of_hanoi(n - 1, auxiliary, target, source)
+
+
+if __name__ == "__main__":
+    tower_of_hanoi(3, "A", "C", "B")
