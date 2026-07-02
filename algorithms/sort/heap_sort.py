1f2afc93b9ab783c68044e0c71f7844a52a2c3d2
Farzan <67397870+farzan-dehbashi@users.noreply.github.com>
2026-07-02 18:35:53 -0400

    feat: add heap sort implementation (#19)

---
+++ algorithms/sort/heap_sort.py
@@ -0,0 +1,27 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+    return arr
+
+
+if __name__ == "__main__":
+    print(heap_sort([12, 11, 13, 5, 6, 7]))
